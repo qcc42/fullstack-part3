fe3a0738d31711bec863fea0c02f28fe614418b1
qcc42 <qcc.1992@gmail.com>
2024-08-23 14:41:15 +0300

    Solution for assignment 3.5., except that id for persons added is given by incrementing the current max ID no., to be fixed.

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (!body.name) {
     return response.status(400).json({ 
       error: 'content missing' 
     })
